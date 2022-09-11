d27b86c61681d2bdd83cb78e86994fb4e30ddd09
thgr8ganzi <q33asq33as@gmail.com>
2022-09-11 15:25:09 +0900

    interaction_design

---
+++ src/main/java/com/study/interaction_design/InteractionDesignApplication.java
@@ -0,0 +1,13 @@
+package com.study.interaction_design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractionDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InteractionDesignApplication.class, args);
+    }
+
+}
