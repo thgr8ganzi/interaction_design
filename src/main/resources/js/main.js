77c39be3377fd30a7b9535a858a157a6886385ec
thgr8ganzi <q33asq33as@gmail.com>
2022-09-14 23:40:25 +0900

    2022-09-14

--- src/main/resources/js/main.js
+++ src/main/resources/js/main.js
@@ -101,7 +101,14 @@
             scrollHeight: 0,
             objs: {
                 container: document.querySelector('#scroll-section-3'),
-                canvasCaption: document.querySelector('.canvas-caption')
+                canvasCaption: document.querySelector('.canvas-caption'),
+                canvas : document.querySelector('.image-blend-canvas'),
+                context: document.querySelector('.image-blend-canvas').getContext('2d'),
+                imagesPath : [
+                    './images/blend-image-1.jpg',
+                    './images/blend-image-2.jpg'
+                ],
+                images: [],
             },
             values: {
 
@@ -122,6 +129,13 @@
             imgElem2 = new Image();
             imgElem2.src = `./video/002/IMG_${7027 + i}.JPG`;
             sceneInfo[2].objs.videoImages.push(imgElem2);
+        }
+
+        let imgElem3;
+        for (let i = 0; i < sceneInfo[3].objs.imagesPath.length; i++) {
+            imgElem3 = new Image();
+            imgElem3.src = sceneInfo[3].objs.imagesPath[i];
+            sceneInfo[3].objs.images.push(imgElem3);
         }
     }
     setCanvasImages();
@@ -283,6 +297,18 @@
 
             case 3:
                 // console.log('3 play');
+                const widthRatio = window.innerWidth / objs.canvas.width;
+                const heightRatio = window.innerHeight / objs.canvas.height
+                let canvasScaleRatio;
+
+                if(widthRatio <= heightRatio){
+                    canvasScaleRatio = heightRatio;
+                }else{
+                    canvasScaleRatio = widthRatio;
+                }
+
+                objs.canvas.style.transform = `scale(${canvasScaleRatio})`;
+                objs.context.drawImage(objs.images[0], 0, 0);
                 break;
         }
     }
